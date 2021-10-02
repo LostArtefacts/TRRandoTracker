97fa88d4cdfa9b2aeba556c6f64318ef16fd417e
lahm86 <33758420+lahm86@users.noreply.github.com>
2021-10-02 17:12:59 +0100

    V0.9.5 Release

--- TR2RandoTracker/Properties/AssemblyInfo.cs
+++ TR2RandoTracker/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("Tracks completed levels when playing TR2 Randomizer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tomp Community")]
-[assembly: AssemblyProduct("TR2RandoTracker v0.9.4")]
+[assembly: AssemblyProduct("TR2RandoTracker v0.9.5")]
 [assembly: AssemblyCopyright("Copyright © Tomp Community 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.4.0")]
-[assembly: AssemblyFileVersion("0.9.4.0")]
+[assembly: AssemblyVersion("0.9.5.0")]
+[assembly: AssemblyFileVersion("0.9.5.0")]
