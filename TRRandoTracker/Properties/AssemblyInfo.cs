6a1b845ec6a4cb2cb3e5808956e9024dc4c25990
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-04-05 20:19:17 +0100

    #26 V1.2.0
    Resolves #26.

--- TRRandoTracker/Properties/AssemblyInfo.cs
+++ TRRandoTracker/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("Tracks completed levels when playing TR Randomizer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tomp Community")]
-[assembly: AssemblyProduct("TRRandoTracker v1.1.0")]
+[assembly: AssemblyProduct("TRRandoTracker v1.2.0")]
 [assembly: AssemblyCopyright("Copyright © Tomp Community 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
