56de5e9f962174ed88212ea212a4c939ef89993e
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-01-19 19:19:58 +0000

    Amend TR1X support (#44)
    Resolves #42.

--- TRRandoTracker.Core/Tomp/Exes/Tomb1Main.cs
+++ TRRandoTracker.Core/Tomp/Exes/Tomb1Main.cs
@@ -22,37 +22,17 @@ public class Tomb1Main : AbstractTompExe
 
     internal override ITracker CreateTracker(Process process, List<AbstractTRScriptedLevel> levels)
     {
-        // 2.11 minimum version
-        _levelAddress = 0x805FAA;
-        _completeAddress = 0xC57449;
+        // 3.0 minimum version
+        _levelAddress = 0x80F58A;
+        _completeAddress = 0xC66629;
         
         Version version = CalculateProductVersion(process.MainModule.FileName);
         if (version != null)
         {
-            if (version >= new Version(3, 0, 3))
+            if (version >= new Version(3, 1))
             {
-                _levelAddress = 0x80F58A;
+                _levelAddress = 0x80F5AA;
                 _completeAddress = 0xC66629;
-            }
-            else if (version >= new Version(2, 16))
-            {
-                _levelAddress = 0x81142A;
-                _completeAddress = 0xC64669;
-            }
-            else if (version >= new Version(2, 15))
-            {
-                _levelAddress = 0x80CFEA;
-                _completeAddress = 0xC5F509;
-            }
-            else if (version >= new Version(2, 14))
-            {
-                _levelAddress = 0x809F8A;
-                _completeAddress = 0xC5C509;
-            }
-            else if (version >= new Version(2, 13))
-            {
-                _levelAddress = 0x808F8A;
-                _completeAddress = 0xC5A509;
             }
         }
         return new Tomb1MainTracker(this, process , levels);
